ef2f1b0db940e7b172366877d71afc9b6cf3bf1b
marshall_lee <drink7036290@gmail.com>
2024-12-11 00:39:27 +0800

    pass --bench-metrics-dir --sub-crate-name --bench-name to bench_util

--- utilities/bench_util/src/modules/db.rs
+++ utilities/bench_util/src/modules/db.rs
@@ -1,10 +1,12 @@
-use anyhow::Ok;
+use anyhow::{anyhow, Ok};
+use clap::Parser;
 use dotenvy::dotenv;
 use reqwest::blocking::Client;
 use serde::Deserialize;
-use std::env;
+use std::fs;
 use std::fs::File;
 use std::io::BufReader;
+use std::{env, path::PathBuf};
 use walkdir::WalkDir;
 
 #[derive(Debug, Deserialize)]
@@ -27,6 +29,105 @@ struct PartialConfidenceInterval {
 pub enum DBStatus {
     Updated,
     NoUpdate,
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Directory path for benchmark metrics like maximum RSS and CPU usage
+    #[arg(long, value_name = "DIR", requires_all = &["sub_crate_name", "bench_name"])]
+    bench_metrics_dir: Option<String>,
+
+    /// Sub-crate name, e.g., "qxxx"
+    #[arg(long, value_name = "SUB_CRATE", requires_all = &["bench_metrics_dir", "bench_name"])]
+    sub_crate_name: Option<String>,
+
+    /// Benchmark name, e.g., "bench_IMPL"
+    #[arg(long, value_name = "BENCH_NAME", requires_all = &["bench_metrics_dir", "sub_crate_name"])]
+    bench_name: Option<String>,
+}
+
+fn move_criterion_dir(
+    criterion_dir: &mut PathBuf,
+    sub_crate_name: &str,
+    bench_name: &str,
+) -> anyhow::Result<()> {
+    // e.g., qxxx_with_blabla
+    let sub_crate_prefix = sub_crate_name.split('_').next().ok_or_else(|| {
+        anyhow!(
+            "Sub-crate name \"{}\" must be in the format of \"qxxx_with_blabla\"",
+            sub_crate_name
+        )
+    })?;
+
+    println!("before criterion_dir: {}", criterion_dir.display());
+
+    // Extend criterion_dir to this specific benchmark
+    criterion_dir.push(format!(
+        "{}_with_{}", // e.g., "qxxx_IMPL"
+        sub_crate_prefix,
+        bench_name.trim_start_matches("bench_")
+    ));
+
+    if criterion_dir.try_exists().is_err() {
+        return Err(anyhow!(
+            "Criterion directory \"{}\" does not exist",
+            criterion_dir.display()
+        ));
+    }
+
+    println!("criterion_dir: {}", criterion_dir.display());
+
+    Ok(())
+}
+
+fn retrieve_metrics_info(criterion_dir: &mut PathBuf) -> anyhow::Result<String> {
+    let mut metrics_info = String::new();
+
+    let args = Args::parse();
+
+    // e.g.,
+    // bench_metrics_dir = /tmp/
+    // sub_crate_name = qxxx_with_blabla
+    // bench_name = bench_IMPL
+    if let (Some(bench_metrics_dir), Some(sub_crate_name), Some(bench_name)) =
+        (args.bench_metrics_dir, args.sub_crate_name, args.bench_name)
+    {
+        println!("bench_metrics_dir: {}", bench_metrics_dir);
+        println!("sub_crate_name: {}", sub_crate_name);
+        println!("bench_name: {}", bench_name);
+
+        move_criterion_dir(criterion_dir, &sub_crate_name, &bench_name)?;
+
+        let bench_metrics_dir = PathBuf::from(bench_metrics_dir);
+        if bench_metrics_dir.try_exists().is_err() {
+            return Err(anyhow!(
+                "Benchmark metrics directory \"{}\" does not exist",
+                bench_metrics_dir.display()
+            ));
+        }
+
+        // for each bench_metrics_dir/FILE, append ",FILE_NAME=FILE_CONTENT" to metrics_info
+        for entry in WalkDir::new(bench_metrics_dir)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.file_type().is_file())
+        {
+            let path = entry.path();
+            let file_name = path.file_name().unwrap().to_string_lossy();
+            let file_content = fs::read_to_string(path)?;
+            metrics_info.push_str(&format!(
+                ",{}={}",
+                file_name,
+                file_content.trim().trim_matches('%')
+            ));
+        }
+    } else {
+        println!("Running without benchmark metrics.");
+    }
+
+    println!("metrics_info: {}", metrics_info);
+
+    Ok(metrics_info)
 }
 
 pub fn update_db() -> anyhow::Result<DBStatus> {
@@ -52,10 +153,10 @@ pub fn update_db() -> anyhow::Result<DBStatus> {
         .to_path_buf();
 
     // Build the path to the target/criterion directory
-    let criterion_dir = workspace_root.join("target").join("criterion");
+    let mut criterion_dir = workspace_root.join("target").join("criterion");
     let mut db_updated = false;
+    let metrics_info = retrieve_metrics_info(&mut criterion_dir)?;
 
-    // Iterate over all estimates.json files
     for entry in WalkDir::new(criterion_dir)
         .into_iter()
         .filter_map(|e| e.ok())
@@ -86,12 +187,13 @@ pub fn update_db() -> anyhow::Result<DBStatus> {
         // Include all relevant statistics
         let line = format!(
             "benchmark,name={},impl={} \
-            slope_lower_bound={},slope_upper_bound={},slope_point_estimate={} {}",
+            slope_lower_bound={},slope_upper_bound={},slope_point_estimate={}{} {}",
             benchmark_name, // name
             impl_name,      // impl
             estimates.slope.confidence_interval.lower_bound,
             estimates.slope.confidence_interval.upper_bound,
             estimates.slope.point_estimate,
+            metrics_info,
             timestamp,
         );
 
