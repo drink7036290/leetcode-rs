cf581512b29c1c69f82f3c979b7ccf4c2a0bec83
marshall_lee <drink7036290@gmail.com>
2024-12-17 23:44:22 +0800

    rustfmt --edition 2024

--- implementations/q460_lfu_cache/src/impl_v3.rs
+++ implementations/q460_lfu_cache/src/impl_v3.rs
@@ -151,13 +151,15 @@ impl LFUCache {
             }
             _ => {
                 if self.map.len() == self.capacity {
-                    if let Some(freq_list) = self.freq_map.get_mut(&self.min_freq) { if let Some(node_rc) = freq_list.pop_front() {
-                        self.map.remove(&node_rc.borrow().key);
+                    if let Some(freq_list) = self.freq_map.get_mut(&self.min_freq) {
+                        if let Some(node_rc) = freq_list.pop_front() {
+                            self.map.remove(&node_rc.borrow().key);
 
-                        if freq_list.is_empty() {
-                            self.freq_map.remove(&self.min_freq);
+                            if freq_list.is_empty() {
+                                self.freq_map.remove(&self.min_freq);
+                            }
                         }
-                    } }
+                    }
                 }
 
                 let node_rc = Rc::new(RefCell::new(Node::new(key, value)));
